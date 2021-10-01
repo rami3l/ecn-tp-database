57bef013d533d99bf80dd5787b860e6a61e7ad04
Jean Tissot <asphjt@gmail.com>
2021-10-01 10:21:40 +0200

    revert zipcode: integer -> string

--- webapp/server/src/main/java/ecn/tp/bddon/server/metier/dto/Address.java
+++ webapp/server/src/main/java/ecn/tp/bddon/server/metier/dto/Address.java
@@ -25,7 +25,7 @@ public class Address implements Serializable {
     @Column(name = "address_line")
     private String addressLine;
 
-    private int zipcode;
+    private String zipcode;
 
     private String city;
 
