2592c17743a7f2e14998d5499d9b4e68c26e2184
Jean Tissot <asphjt@gmail.com>
2021-10-02 23:11:23 +0200

    add mission card

--- webapp/client/src/app/app-routing.module.ts
+++ webapp/client/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MissionCardComponent } from './components/content/mission/mission-card/mission-card.component';
 import { MissionsComponent } from './components/content/mission/missions/missions.component';
+import { OrderCardComponent } from './components/content/order/order-card/order-card.component';
 import { OrdersComponent } from './components/content/order/orders/orders.component';
 import { StockComponent } from './components/content/stock/stock.component';
 import { WelcomeComponent } from './components/content/welcome/welcome.component';
@@ -8,9 +10,11 @@ import { WelcomeComponent } from './components/content/welcome/welcome.component
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
 
-  { path: 'missions', component: MissionsComponent },
+  { path: 'mission/list', component: MissionsComponent },
+  { path: 'mission/see/:id', component: MissionCardComponent },
 
-  { path: 'orders', component: OrdersComponent },
+  { path: 'order/list', component: OrdersComponent },
+  { path: 'order/see/:id', component: OrderCardComponent },
 
   { path: 'stock', component: StockComponent }
 ];
--- webapp/client/src/app/components/content/mission/mission-card/mission-card.component.ts
+++ webapp/client/src/app/components/content/mission/mission-card/mission-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Mission } from 'src/app/dto/mission';
+import { MissionService } from 'src/app/services/rest/mission.service';
 
 @Component({
   selector: 'app-mission-card',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MissionCardComponent implements OnInit {
 
-  constructor() { }
+  mission: Mission | undefined;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private missionService: MissionService) { }
 
   ngOnInit(): void {
+    var string_id = this.activatedRoute.snapshot.paramMap.get('id');
+    var id = string_id ? +string_id : NaN;
+    if (isNaN(id)) {
+      //TODO: exception
+    } else {
+      this.missionService.getMission(id).subscribe(
+        missionReceived => { this.mission = missionReceived; }
+      )
+    }
   }
 
 }
