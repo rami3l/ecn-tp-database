ddd7045858e1c0db8ee98158319116662cec1b7a
Jean Tissot <asphjt@gmail.com>
2021-10-04 11:47:49 +0200

    update mission

--- webapp/client/src/app/dto/mission.ts
+++ webapp/client/src/app/dto/mission.ts
@@ -1,10 +1,12 @@
 import { Driver } from "./driver";
+import { LoadingPoint } from "./loadingpoint";
 import { Truck } from "./truck";
 
 export class Mission {
     constructor(
         public id: number,
         public loadingTime: Date,
+        public loadingPoint: LoadingPoint,
         public driver: Driver,
         public truck: Truck
     ) { }
