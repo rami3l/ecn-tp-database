ab6d6da387c22a120898ba4c14e47910e47864e8
rami3l <rami3l@outlook.com>
2021-10-09 09:39:59 +0200

    fix: fix syntax breakage due to formatter issues

--- env_sql/scripts/31-msg09.sql
+++ env_sql/scripts/31-msg09.sql
@@ -1,41 +1,34 @@
 -- Q9. Add the triggers on stock when we create, cancel or modify an order.
-CREATE
-OR REPLACE FUNCTION public.stock_update() RETURNS trigger LANGUAGE 'plpgsql' NOT LEAKPROOF AS $ BODY $ begin -- new :: order_content, influence :: stock
-update
-    stock as s
-set
-    quantity = s.quantity + (
-        case
-            when tg_op = 'INSERT' then - new.quantity
-            when tg_op = 'UPDATE' then old.quantity - new.quantity
-            when tg_op = 'DELETE' then old.quantity
-        end
-    )
-from
-    product p,
-    order_content o,
-    supported_by sup,
-    mission m
-where
-    o.product = product_id
-    and product_id = s.product
-    and sup.order_content = order_content_id
-    and sup.mission = mission_id
-    and s.loading_point = m.loading_point;
+
+CREATE OR REPLACE FUNCTION public.stock_update() RETURNS trigger LANGUAGE 'plpgsql' NOT LEAKPROOF AS $BODY$
+begin
+-- new :: order_content, influence :: stock
+update stock as s
+set quantity = s.quantity + (case
+ when tg_op = 'INSERT' then -new.quantity
+ when tg_op = 'UPDATE' then old.quantity - new.quantity
+ when tg_op = 'DELETE' then old.quantity
+ end)
+from product p, order_content o, supported_by sup, mission m
+where o.product = product_id
+  and product_id = s.product
+  and sup.order_content = order_content_id
+  and sup.mission = mission_id
+  and s.loading_point = m.loading_point;
 
 return new;
-
 end;
+$BODY$;
 
-$ BODY $;
 
 ALTER FUNCTION public.stock_update() OWNER TO postgres;
 
-CREATE TRIGGER stock_update
-AFTER
-    DELETE
-    OR
+
+CREATE TRIGGER stock_update AFTER
+DELETE
+OR
 UPDATE
-    OR
-INSERT
-    ON public.order_content FOR EACH ROW EXECUTE FUNCTION public.stock_update();
+OR
+INSERT ON public.order_content
+FOR EACH ROW EXECUTE FUNCTION public.stock_update();
+
