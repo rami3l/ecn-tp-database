2795d6c897baf09c348b31e74e92290f5218206e
Jean Tissot <asphjt@gmail.com>
2021-10-13 13:38:52 +0200

    add loadingPoint, driver & truck selectors

--- webapp/client/src/app/app.module.ts
+++ webapp/client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
@@ -32,7 +32,8 @@ import { NavigationComponent } from './components/navigation/navigation.componen
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webapp/client/src/app/components/content/mission/mission-form/mission-form.component.ts
+++ webapp/client/src/app/components/content/mission/mission-form/mission-form.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MissionToSave } from 'src/app/dto/creations/mission-to-save';
+import { Driver } from 'src/app/dto/driver';
 import { LoadingPoint } from 'src/app/dto/loadingpoint';
 import { SupportedBy } from 'src/app/dto/supportedby';
+import { Truck } from 'src/app/dto/truck';
 import { PlacesService } from 'src/app/services/rest/places.service';
+import { TransportService } from 'src/app/services/rest/transport.service';
 
 @Component({
   selector: 'app-mission-form',
@@ -11,16 +15,55 @@ import { PlacesService } from 'src/app/services/rest/places.service';
 })
 export class MissionFormComponent implements OnInit {
 
+  useDefaultTruck = true;
   mission: MissionToSave = new MissionToSave();
   supports: SupportedBy[] = [];
   loadingPoints: LoadingPoint[] = [];
+  drivers: Driver[] = [];
+  trucks: Truck[] = [];
+  missionForm = new FormGroup({
+    loadingTime: new FormControl(''),
+    loadingPoint: new FormControl('', Validators.required),
+    driver: new FormControl('', Validators.required),
+    truck: new FormControl({ value: '', disabled: true }),
+  });
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService,
+    private transportService: TransportService) { }
 
   ngOnInit(): void {
     this.placesService.getLoadingPoints().subscribe(
       loadingPointsReceived => { this.loadingPoints = loadingPointsReceived; }
     )
+    this.transportService.getDrivers().subscribe(
+      driversReceived => { this.drivers = driversReceived; }
+    )
+    this.transportService.getTrucks().subscribe(
+      trucksReceived => { this.trucks = trucksReceived; }
+    )
+
+
+  }
+
+  onSubmit(): void {
+    console.log(this.missionForm.value);
+  }
+
+  onDriverChange(): void {
+    if (this.useDefaultTruck) {
+      var driverId = parseInt(this.missionForm.controls.driver.value);
+      var driverSelected = this.drivers.filter(driver => (driver.id == driverId))[0];
+      this.missionForm.controls.truck.setValue(driverSelected ? driverSelected.defaultTruck.licensePlate : "");
+    }
+  }
+
+  toggleUseDefaultTruck() {
+    this.useDefaultTruck = !this.useDefaultTruck;
+    if (this.useDefaultTruck) {
+      this.missionForm.controls["truck"].disable();
+    } else {
+      this.missionForm.controls["truck"].enable();
+    }
   }
 
 
