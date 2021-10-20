7b46cc033f20db4bd49a9bd16ca544fb48467e6c
Jean Tissot <asphjt@gmail.com>
2021-10-20 19:08:49 +0200

    fix: add certifiedfor idclass annotation

--- webapp/server/src/main/java/ecn/tp/bddon/server/metier/dto/CertifiedFor.java
+++ webapp/server/src/main/java/ecn/tp/bddon/server/metier/dto/CertifiedFor.java
@@ -5,13 +5,16 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.Table;
 
+import ecn.tp.bddon.server.metier.dto.id_classes.CertifiedForId;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "certified_for")
+@IdClass(CertifiedForId.class)
 @Data
 @NoArgsConstructor
 public class CertifiedFor implements Serializable {
